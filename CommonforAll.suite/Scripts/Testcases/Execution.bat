bdc2783b92dbe63d68b3ce6871c708cbd708ea96
sudheekshaIdeabytes <sudheeksha.gunuputi@ideabytes.com>
2025-06-20 12:49:03 +0530

    Update Execution.bat

--- CommonforAll.suite/Scripts/Testcases/Execution.bat
+++ CommonforAll.suite/Scripts/Testcases/Execution.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Eggplant\Eggplant.bat" "C:\Users\sudheeksha.gunuputi\Documents\Eggplant\CommonforAll.suite\Scripts\Testcases\LaunchApplication.script"
+"C:\Program Files\Eggplant\Eggplant.bat" 
+"C:\Users\sudheeksha.gunuputi\Documents\Eggplant\CommonforAll.suite\Scripts\Testcases\LaunchApplication.script"
