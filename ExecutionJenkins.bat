25e559e8d3d6f66bc80a9bb70b3f39394d145fa2
sudheekshaIdeabytes <sudheeksha.gunuputi@Ideabytes.com>
2025-06-23 12:40:02 +0530

    Updated .bat file with local script path instead of git script path

--- ExecutionJenkins.bat
+++ ExecutionJenkins.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Eggplant\Eggplant.bat" "C:\Users\sudheeksha.gunuputi\Documents\Eggplant\CommonforAll.suite\Scripts\LaunchApplication.script"
+"C:\Program Files\Eggplant\Eggplant.bat" "C:\Users\sudheeksha.gunuputi\Documents\Eggplant\CommonforAll.suite\Scripts\testcases\LaunchApplication.script"
